e16e2beddd44ec6f5f29fdb7add9b2cc684e47ec
jeishu <jeremy.zhu@utexas.edu>
2021-02-09 21:31:02 -0600

    Added images for projects and profile picture for about me

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 new Glider(document.querySelector('.glider'), {
   slidesToShow: 1,
+  slidesToScroll: 'auto',
   dots: '#dots',
   draggable: true,
+  rewind: true,
   arrows: {
     prev: '.glider-prev',
     next: '.glider-next'
